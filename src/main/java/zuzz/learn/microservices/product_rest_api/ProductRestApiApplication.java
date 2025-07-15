4a76900f414158e1fc3fe5b58a59602abdd40386
johaoDev <johaobravoespinoza1501@gmail.com>
2025-07-15 12:14:08 -0500

    initial commit

---
+++ src/main/java/zuzz/learn/microservices/product_rest_api/ProductRestApiApplication.java
@@ -0,0 +1,13 @@
+package zuzz.learn.microservices.product_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRestApiApplication.class, args);
+	}
+
+}
